68882ec588b1b7d0e0f92777663834a128f22aef
JohnnyEmmanuel <esquare.jay@gmail.com>
2026-04-15 13:03:38 +0100

    completed the header of the landing page and made some edits in the login and signup page

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
+console.log("Hello world!");
+
 function initNav(barId, linksId, containerId) {
   const bar = document.getElementById(barId);
   const links = document.getElementById(linksId);
   const container = document.getElementById(containerId);
+ console.log(bar, 'debugging');
 
   if (!bar || !links || !container) return;
-
+  
   bar.addEventListener("click", () => {
+
     links.classList.toggle("hidden");
+    links.classList.toggle("flex");
     container.classList.toggle("h-32");
     container.classList.toggle("h-96");
   });
+  console.log(bar);
 }
 
 initNav("nav-bar-signup", "nav-signup", "nav-container-signup");
 initNav("nav-bar-login", "nav-login", "nav-container-login");
+initNav("nav-bar-landing", "nav-landing", "nav-container-landing");

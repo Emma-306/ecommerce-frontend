ae1f10d469ab6f2e3b0453e016e72e90a920ba90
JohnnyEmmanuel <esquare.jay@gmail.com>
2026-04-23 12:17:23 +0100

    updates on checkout

--- js/checkout.js
+++ js/checkout.js
@@ -2,7 +2,17 @@ import { getWishlist, getCart, saveCart } from "./data.js";
 
 
 export function setupCheckoutPage(){
+    const firstName = document.getElementById("firstName").value;
+    const companyName = document.getElementById("companyName").value;
+    const streetAddress = document.getElementById("streetAddress").value;
+    const apartment = document.getElementById("apartment").value;
+    const city = document.getElementById("city").value;
+    const phoneNumber = document.getElementById("phoneNumber").value;
+    const email = document.getElementById("email").value;
+    const saveDetails = document.getElementById("saveDetails").checked;
     updateCartLength();
+    getOrderDetails();
+    updatesummary();
 }
 
 function updateCartLength() {
@@ -23,4 +33,88 @@ function updateCartLength() {
     cartLengthElement.textContent =
       cart.length.toString();
   }
+}
+
+function getOrderDetails(){
+    const cart = getCart();
+    const orderDetailsContainer = document.getElementById("order-details");
+    if(!orderDetailsContainer) return;
+    orderDetailsContainer.innerHTML = "";
+    cart.forEach((item)=>{
+        orderDetailsContainer.innerHTML += `
+        <div class="flex flex-row justify-between items-center mb-6">
+            <div class="flex flex-row gap-4 items-center">
+                <img src="${item.image}" alt="product image" class="h-12 w-12 object-contain">
+                <span class="text-base">${item.name}</span>
+            </div>
+            <span class="text-base">$${Math.round(getDiscountedPrice(item)) * Number(item.quantity)}</span>
+         </div>
+        `;
+    });
+}
+
+function getDiscountedPrice(product) {
+  if (product.discount === 0) {
+    return product.price;
+  }
+
+  const discountAmount =
+    (product.price * product.discount) / 100;
+
+  return product.price - discountAmount;
+}
+
+
+function updatesummary() {
+  const subTotalSum = document.getElementById("subtotal-sum");
+  const shippingFee = document.getElementById("shipping-fee");
+  const shippingPrice = document.getElementById("total-price");
+  const cart = getCart();
+  let subtotal = 0;
+  cart.forEach((item) => {
+    const discountedPrice = getDiscountedPrice(item);
+    subtotal += discountedPrice * item.quantity;
+  });
+
+  const shipping = getShippingFee(subtotal);
+
+  subTotalSum.textContent =`${Math.round(subtotal)}`;
+  shippingFee.textContent = shipping;
+  shippingPrice.textContent = `${Math.round(subtotal) + (shipping === "Free" ? 0 : shipping)}`;
+
+  if (cart.length === 0) {
+    subTotalSum.textContent = "0.00";
+    shippingPrice.textContent = "0.00";
+  }
+}
+
+function getShippingFee(cartTotal) {
+  if (cartTotal >= 100) {
+    return "Free";
+  }
+  return 10;
+}
+
+function validateForm(){
+    if (!email.includes("@")) {
+        alert("Enter a valid email");
+        return;
+    }
+    if (saveDetails) {
+
+    const userData = {
+        firstName,
+        companyName,
+        streetAddress,
+        apartment,
+        city,
+        phoneNumber,
+        email
+    };
+
+    localStorage.setItem(
+        "checkoutDetails",
+        JSON.stringify(userData)
+    );
+    }
 }
